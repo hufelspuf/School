de026aa2db4a71a781630eb827cbaebc6e7c908a
Hufelspuf <julian-knook@hotmail.com>
2023-04-03 01:32:04 +0200

    update savegame (Execution time: 7002.164 s)

--- The Witness autocommit V2.bat
+++ The Witness autocommit V2.bat
@@ -2,25 +2,19 @@
 set GameExe=witness64_d3d11.exe
 set SavePath=C:\Users\julia\AppData\Roaming\The Witness
 set GamePath=where witness64_d3d11.exe
+
 cd /d %SavePath%
 git pull
 
-FOR /F "tokens=* USEBACKQ" %%F IN (`ptime.exe %GameExe%`) DO (
-    SET var=%%F
-)
  
 echo Starting "%GameExe%"...
 for /f "delims=" %%a in ('%GamePath%') do set MY_DIR=%%~dpa
 cd /d %MY_DIR%
 echo Current directory is: %CD%
-start "" "%GameExe%"
 
-echo Waiting for "%GameExe%" to close...
-:LOOP
-tasklist /nh /fi "imagename eq %GameExe%" | find /i "%GameExe%" > nul
-if not errorlevel 1 (
-    timeout /t 1 > nul
-    goto LOOP
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`ptime.exe %GameExe%`) DO (
+    SET var=%%F
 )
 
 echo "%GameExe%" has been closed. Performing post-exit actions...
