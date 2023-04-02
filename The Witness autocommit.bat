0ce1a420a1ef7c4c830d2690aa2aa5cabd82e397
Hufelspuf <julian-knook@hotmail.com>
2023-04-02 22:08:27 +0200

    update savegame (Execution time: 0.061 s)

--- The Witness autocommit.bat
+++ The Witness autocommit.bat
@@ -4,7 +4,6 @@ git pull
 
 cd /d C:\Users\julia\Desktop
 
-for /f "delims=" %%a in ('where witness64_d3d11.exe') do taskkill /im "%%~nxa" /f
 
 FOR /F "tokens=* USEBACKQ" %%F IN (`ptime.exe where witness64_d3d11.exe`) DO (
 SET var=%%F
