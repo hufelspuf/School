9138203a2daddc6262e9b9215f044d299e554b15
Hufelspuf <julian-knook@hotmail.com>
2023-04-02 23:33:19 +0200

    update savegame (Execution time: 26.221 s)

--- test.bat
+++ test.bat
@@ -1,7 +1,26 @@
 @echo off
-set MY_DIR_PATH=where witness64_d3d11.exe
-for /f "delims=" %%a in ('%MY_DIR_PATH%') do set MY_DIR=%%~dpa
+set GameExe=witness64_d3d11.exe
+set SavePath=C:\Users\julia\AppData\Roaming\The Witness
+set GamePath=where witness64_d3d11.exe
+cd /d %SavePath%
+git pull
+
+
+ 
+echo Starting "%GameExe%"...
+for /f "delims=" %%a in ('%GamePath%') do set MY_DIR=%%~dpa
 cd /d %MY_DIR%
 echo Current directory is: %CD%
 
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`ptime.exe %GameExe%`) DO (
+    SET var=%%F
+)
+
+echo "%GameExe%" has been closed. Performing post-exit actions...
+cd /d %SavePath%
+git add .
+git commit -m "update savegame (%var%)"
+git push
 pause
+
